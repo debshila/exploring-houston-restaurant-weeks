18ab387f57edc8d8ab17307077459e52a29612df
debshila <debshila@gmail.com>
2019-08-18 21:12:14 -0500

    Clustered items based on pca

--- hrw_ml.R
+++ hrw_ml.R
@@ -1,7 +1,7 @@
 # Word2vec for menu description -------------------------------------------
 library(h2o)
 library(tm)
-
+library(tidyverse)
 all_menus <- read_csv("~/Box Sync/hrw_menus.csv")
 
 all_menus <- all_menus %>%
@@ -13,11 +13,11 @@ all_menus <- all_menus %>%
 h2o.init()
 
 STOP_WORDS <- c(stopwords("en"), "na")
-string_to_tokenize <- as.h2o(all_menus$item_desc[!is.na(all_menus$item_desc)])
-tokenize_string <- h2o.tokenize(as.character(string_to_tokenize), ", ")
 
 tokenize <- function(string_to_tokenize, stop.words = STOP_WORDS) {
     tokenized <- h2o.tokenize(as.character(string_to_tokenize), "_")
+    # convert to lower case
+    tokenized.lower <- h2o.tolower(tokenized)
 
     # remove short words (less than 2 characters)
     tokenized.lengths <- h2o.nchar(tokenized.lower)
@@ -29,23 +29,35 @@ tokenize <- function(string_to_tokenize, stop.words = STOP_WORDS) {
     tokenized.words[is.na(tokenized.words) || (! tokenized.words %in% STOP_WORDS),]
 }
 
+string_to_tokenize <- as.h2o(all_menus$item_desc[!is.na(all_menus$item_desc)])
+tokenize_string <- h2o.tokenize(as.character(string_to_tokenize), ", ")
+
 words <- tokenize(string_to_tokenize = string_to_tokenize)
 print("Build word2vec model")
 w2v.model <- h2o.word2vec(words,
                           model_id = "w2v_model",
-                          vec_size = 20,
+                          vec_size = 10,
                           min_word_freq = 1,
                           window_size = 5,
                           init_learning_rate = 0.025,
                           sent_sample_rate = 0,
                           epochs = 300)
 
-print("Sanity check - find synonyms for the word 'mango'")
+print("Sanity check - find synonyms for the word 'pasta'")
 print(h2o.findSynonyms(w2v.model, "pasta", count = 10))
 
-item_desc_vecs <- h2o.transform(w2v.model, words, aggregate_method = "AVERAGE")
+item_desc_vecs <- h2o.transform(w2v.model, words, aggregate_method = "NONE")
 
 
+data <- h2o.cbind(words,
+                  item_desc_vecs)
+df <- as_tibble(data) %>%
+    filter(!is.na(C1)) %>%
+    rename(item = C1, C1 = C10.1)
+df %>%
+    write_csv("~/Box Sync/hrw/word_vec_item_desc.csv")
+
+df <- read_csv("~/Box Sync/hrw/word_vec_item_desc.csv")
 # `predict` conflicts with generic fn defined in R.stats
 .predict <- function(item, w2v, gbm) {
     words <- tokenize(as.character(as.h2o(item)))
@@ -53,6 +65,23 @@ item_desc_vecs <- h2o.transform(w2v.model, words, aggregate_method = "AVERAGE")
     h2o.predict(gbm, item.vec)
 }
 
+# Clustering with pca
+pca <- prcomp(df[2:ncol(df)], rank = 3)
 
+library(ggiraph)
+plot1 <- data.frame(item = df$item, pca$x) %>%
+    ggplot(aes(x = PC1, y = PC2, colour = PC3)) +
+    geom_point_interactive(alpha = .4, aes(tooltip = item)) +
+    theme_minimal()
 
+girafe(ggobj = plot1)
+
+df_clust <- df %>%
+    select(item) %>%
+    bind_cols(cluster = kmeans_cluster$cluster)
+
+df_clust %>%
+    ggplot(aes(cluster))
+
+# ref: https://github.com/h2oai/h2o-3/blob/master/h2o-r/demos/rdemo.word2vec.craigslistjobtitles.R
 
